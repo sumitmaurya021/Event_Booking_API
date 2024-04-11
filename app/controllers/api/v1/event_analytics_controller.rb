0f1ed8e64248a80e8d327c8494133ef34f40ccff
sumit <mauryasumit222@gmail.com>
2024-04-11 17:50:57 +0530

    [ 11 April ] [" Fixing Error Handling "]

--- app/controllers/api/v1/event_analytics_controller.rb
+++ app/controllers/api/v1/event_analytics_controller.rb
@@ -2,6 +2,10 @@ class Api::V1::EventAnalyticsController < ApplicationController
   skip_before_action :doorkeeper_authorize!, only: [:download_pdf]
   def download_pdf
     @event = Event.find(params[:event_id])
+    unless @event
+      render json: { error: "Event not found" }, status: :not_found
+      return
+    end
     @speaker = @event.speakers
     respond_to do |format|
       format.html
